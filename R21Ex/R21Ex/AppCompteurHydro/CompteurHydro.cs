96d9fdf31fad08ffbefff18f4e0a9340ec182da3
Vincent Gagnon <2480477@cegepmontpetit.ca>
2025-04-24 14:00:15 -0400

    FCT : Ajout de la classe CompteurHydro
    Ajout de la classe CompteurHydro qui implémente l’interface ICompteurHydro

---
+++ R21Ex/R21Ex/AppCompteurHydro/CompteurHydro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCompteurHydro
+{
+	public class CompteurHydro : ICompteurHydro
+	{
+		
+		private int m_consommationActuelle;
+		
+		
+		public int ConsommationActuelle
+		{
+			get { return m_consommationActuelle;  }
+		}
+
+
+		public CompteurHydro()
+		{
+			m_consommationActuelle = 0;
+		}
+
+
+		/// <summary>
+		/// Mettre a jour la consomation actuelle selon la quantité consommée
+		/// </summary>
+		/// <param name="pQuantitéKwh"></param>
+		
+		public void Consommer(int pQuantitéKwh)
+		{
+			m_consommationActuelle -= pQuantitéKwh;
+		}
+	}
+}
